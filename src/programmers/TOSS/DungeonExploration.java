2ce80cb2c10c5b1636036d8aeeb1da140410e405
seongjo24 <kangsj24@naver.com>
2022-08-08 23:59:50 +0900

    20220808

--- src/programmers/TOSS/DungeonExploration.java
+++ src/programmers/TOSS/DungeonExploration.java
@@ -34,4 +34,6 @@ public class DungeonExploration {
         }
         return max;
     }
+
+
 }
---
+++ src/programmers/tipsdown/RemoveByPair.java
@@ -0,0 +1,30 @@
+package programmers.tipsdown;
+
+import java.util.Stack;
+
+public class RemoveByPair {
+    public int solution(String s) {
+        int answer = -1;
+        char[] carr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < carr.length; i++) {
+            char c = carr[i];
+
+            if (stack.isEmpty()) stack.push(c);
+
+            else {
+
+                if (stack.peek() == c) {
+                    stack.pop();
+
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
+
