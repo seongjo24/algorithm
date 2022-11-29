1674da3cfa0032fef924fc1abf7ac87586732ee9
seongjo24 <kangsj24@naver.com>
2022-11-30 01:15:24 +0900

    20221003

--- src/programmers/practice/TravelRoute.java
+++ src/programmers/practice/TravelRoute.java
@@ -1,4 +1,43 @@
 package programmers.practice;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TravelRoute {
+    int ticketSize;
+    String[][] tickets;
+    ArrayList<String> answerList;
+    boolean[] visit;
+
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        ticketSize = tickets.length;
+        this.tickets = tickets;
+        visit = new boolean[ticketSize];
+        answerList = new ArrayList<>();
+        dfs("ICN", "", 0);
+        Collections.sort(answerList);
+
+        answer = answerList.get(0).split(" ");
+        return answer;
+    }
+
+    public void dfs(String now, String strAnswer, int cnt) {
+        strAnswer += (now + " ");
+        if (cnt == ticketSize) {
+            answerList.add(strAnswer);
+            return;
+        }
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visit[i] && tickets[i][0].equals(now)) {
+                visit[i] = true;
+                dfs(tickets[i][1], strAnswer, cnt + 1);
+                visit[i] = false;
+            }
+
+        }
+
+
+    }
 }
