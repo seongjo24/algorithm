7052a5e5f238c2833dc0138b3ce974b7b967c6ff
seongjo24 <kangsj24@naver.com>
2022-12-26 23:27:07 +0900

    3트

--- src/programmers/practice/SplitThePowerGridInTwo.java
+++ src/programmers/practice/SplitThePowerGridInTwo.java
@@ -1,55 +1,40 @@
 package programmers.practice;
 
-import java.util.*;
 class SplitThePowerGridInTwo {
-    int min;
-    int N;
     boolean[][] edges;
     boolean[] visited;
+    int min = Integer.MAX_VALUE;
+
     public int solution(int n, int[][] wires) {
-        N=n+1;
+        int N = n + 1;
         edges = new boolean[N][N];
-        visited= new boolean[N];
-        int min= Integer.MAX_VALUE;
-        for(int i =0;i<wires.length;i++){
-
-            int a= wires[i][0];
-            int b= wires[i][1];
-            edges[a][b]=true;
-            edges[b][a]=true;
+        visited = new boolean[N];
+        for (int i = 0; i < wires.length; i++) {
+            edges[wires[i][0]][wires[i][1]] = true;
+            edges[wires[i][1]][wires[i][0]] = true;
         }
-        for(int i =0;i<wires.length;i++){
-            Arrays.fill(visited, false);
-            int a= wires[i][0];
-            int b= wires[i][1];
-            edges[a][b]=false;
-            edges[b][a]=false;
-
-            int aCnt= dfs(a,0);
-            int bCnt= dfs(b,0);
-            min= Math.min(min,Math.abs(aCnt-bCnt));
-            edges[a][b]=true;
-            edges[b][a]=true;
-
+        for (int i = 0; i < wires.length; i++) {
+            edges[wires[i][0]][wires[i][1]] = false;
+            edges[wires[i][1]][wires[i][0]] = false;
+            min = Math.min(min, Math.abs(dfs(wires[i][0]) - dfs(wires[i][1])));
+            edges[wires[i][0]][wires[i][1]] = true;
+            edges[wires[i][1]][wires[i][0]] = true;
         }
-
         return min;
     }
 
-    public int dfs(int cur,int cnt){
-        if(visited[cur]) return 0;
 
-        visited[cur]=true;
-
-        int sum = 1;
-
-        for(int i=0; i<N;i++){
-            if(!edges[cur][i]){
-                continue;
-            }
-            sum+=dfs(i,cnt+1);
+    public int dfs(int edge) {
+        if (visited[edge]) {
+            return 0;
         }
-        visited[cur] = false;
-        return sum;
+        visited[edge] = true;
+        int cnt = 1;
+        for (int i = 0; i < edges[edge].length; i++) {
+            if (edges[edge][i])
+                cnt += dfs(i);
+        }
+        visited[edge] = false;
+        return cnt;
     }
 }
